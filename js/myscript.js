fe73f66e8228112fff89c68e56328739241cf192
sofia-papa <papa.sofia@outlook.it>
2021-09-01 09:31:18 +0100

    pari e dispari

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,5 @@
+//palindroma
+
 let parola = prompt("inserisci una parola");
 let parolaPali = invertiParola(parola);
 
@@ -11,3 +13,30 @@ function invertiParola(str){
   var strInversa = str.split('').reverse().join('');  
   return strInversa;
 }
+
+//pari e dispari
+
+let numero = parseInt(prompt("Inserisci un numero"))
+
+for (i = 0; i < 5; i++ ){
+        numeroComputer = getRandomInt(1,5);
+    }
+    console.log(numeroComputer);
+
+
+//funzione per ottenere un numero randomico 
+function getRandomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function IsNotUneven(numero){
+    if (isNaN(numero) == false){
+        return (numero %2 == 1 ?  true : false);
+    } else {
+        return null;
+    }
+}
+
+console.log(numero);
